957506fa755c36782108ee7397a23d300619e40c
mjp0997 <mjp0997@gmail.com>
2022-11-01 18:34:51 -0400

    Separando layout de welcome, creando vista principal

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -3,12 +3,22 @@
       <!-- Bread crumbs -->
       <nav aria-label="breadcrumb">
          <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-            <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Pages</a></li>
-            
-            <li class="breadcrumb-item text-sm text-white active" aria-current="page">Dashboard</li>
+            <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">FarmaPOST</a></li>
+
+            @foreach ($bread as $path)
+               <li
+                  class="breadcrumb-item text-sm text-white"
+                  aria-current="page"
+               >
+                  <a
+                     href='{{ url($path['link']) }}'
+                     class="text-sm text-white @if($loop->last) active @endif"
+                  >{{ $path['text'] }}</a>
+               </li>
+            @endforeach
          </ol>
 
-         <h6 class="font-weight-bolder text-white mb-0">Dashboard</h6>
+         <h6 class="font-weight-bolder text-white mb-0 text-uppercase">{{ $bread[count($bread) - 1]['text'] }}</h6>
       </nav>
 
       <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,11 +1,11 @@
 <aside class="sidenav bg-white navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-4 " id="sidenav-main">
-   <div class="sidenav-header">
+   <div>
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
 
-      <a class="navbar-brand m-0" href=" https://demos.creative-tim.com/argon-dashboard/pages/dashboard.html " target="_blank">
-         <img src="./assets/img/logo-ct-dark.png" class="navbar-brand-img h-100" alt="main_logo">
+      <a class="navbar-brand d-flex flex-column align-items-center m-0" href="{{ url('/') }}">
+         <p class="farma-font h3 m-0 lh-1">Farma</p>
 
-         <span class="ms-1 font-weight-bold">Argon Dashboard 2</span>
+         <p class="post-font h2 m-0 lh-1 text-primary">POST</p>
       </a>
    </div>
 
