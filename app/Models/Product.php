46b1416033b2a7e89bf652795fffa058aff86537
mjp0997 <mjp0997@gmail.com>
2022-11-03 18:23:32 -0400

    Minificando

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,8 +8,7 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Product extends Model
 {
-    use HasFactory;
-    use SoftDeletes;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = ['name', 'buy_price', 'sell_price'];
 }
