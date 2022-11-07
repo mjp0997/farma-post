1dbf738c6805eafc3ec14b8e69059a9159474079
mjp0997 <mjp0997@gmail.com>
2022-11-07 09:39:05 -0400

    Botón de cerrar sesión en panel de venta

---
+++ resources/views/components/operation-logout.blade.php
@@ -0,0 +1,24 @@
+<form
+   method="POST"
+   action="{{ url('/logout') }}"
+   class="btn btn-warning text-white font-weight-bold operation-logout"
+   id="logout"
+>
+   @csrf
+
+   @method('POST')
+
+   <i class="fa fa-sign-out me-sm-1"></i>
+
+   <span type="submit" class="d-sm-inline d-none">Cerrar sesión</span>
+</form>
+   
+<script type="text/javascript">
+   document.addEventListener("DOMContentLoaded", () => {
+      const logout = document.querySelector('#logout');
+
+      logout.addEventListener('click', () => {
+         logout.submit();
+      });
+   });
+</script>
