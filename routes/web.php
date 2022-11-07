824422d74b116b1414de9176c86f6198996b5d1d
mjp0997 <mjp0997@gmail.com>
2022-11-07 11:33:39 -0400

    Agregando rutas de clientes

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ClientsController;
 use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\HomeController;
@@ -35,6 +36,8 @@ Route::group(['middleware' => 'auth'], function () {
       Route::resource('/products', ProductsController::class);
 
       Route::resource('/users', UsersController::class);
+
+      Route::resource('/clients', ClientsController::class);
    });
 
    Route::get('/sale', [OperationController::class, 'index']);
