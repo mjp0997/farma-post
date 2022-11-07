0262b5877874699a9b23f70ab0e4086285fde973
mjp0997 <mjp0997@gmail.com>
2022-11-07 11:09:11 -0400

    Agregado enlace a panel de ventas

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -32,6 +32,19 @@
 
          <li class="nav-item">
             <a
+               class="nav-link {{ request()->is('sale*') ? 'active' : '' }}"
+               href="{{ url('/sale') }}"
+            >
+               <div class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
+                  <i class="ni ni-cart text-danger text-sm opacity-10"></i>
+               </div>
+               
+               <span class="nav-link-text ms-1">Panel de venta</span>
+            </a>
+         </li>
+
+         <li class="nav-item">
+            <a
                class="nav-link {{ request()->is('products*') ? 'active' : '' }}"
                href="{{ url('/products') }}"
             >
