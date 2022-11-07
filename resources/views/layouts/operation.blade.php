661f1e3d21f42fa46a47e4fcfb80a2ef6974891c
mjp0997 <mjp0997@gmail.com>
2022-11-07 11:08:26 -0400

    Layout de panel de ventas

---
+++ resources/views/layouts/operation.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+@include('components.head')
+
+<body class="g-sidenav-show bg-gray-100">
+   <div class="min-height-300 bg-primary position-absolute w-100"></div>
+
+   <main class="main-content position-relative border-radius-lg">
+      @include('components.nav')
+
+      <div class="container-fluid py-4">
+         @yield('content')         
+      </div>
+   </main>
+   
+   @include('components.footer')
+</body>
+
+</html>
