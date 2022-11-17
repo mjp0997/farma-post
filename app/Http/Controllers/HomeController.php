3b526bf53bf810e70691b6feacbe8a5dcb6e181a
mjp0997 <mjp0997@gmail.com>
2022-11-17 04:06:55 -0400

    Información de los recuadros de datos del dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Sale;
+use App\Models\SalesLine;
+use Illuminate\Support\Facades\Auth;
 
 class HomeController extends Controller
 {
@@ -13,7 +15,11 @@ class HomeController extends Controller
      */
     public function index()
     {
-        // TODO: verificar rol del usuario autenticado y redireccionar
+        $user = Auth::user();
+
+        if ($user->role->name == 'USER') {
+            return redirect('/sale');
+        }
 
         $bread = [
             [
@@ -22,8 +28,30 @@ class HomeController extends Controller
             ]
         ];
 
+        $current_date = date('Y-m-d');
+
+        $today_sales_lines = SalesLine::where('created_at', '>=', $current_date)->get();
+
+        $earnings = 0;
+
+        $today_sales = 0;
+
+        foreach ($today_sales_lines as $sales_line) {
+            $earnings += ($sales_line->current_price - $sales_line->current_buy_price) * $sales_line->quantity;
+
+            $today_sales += $sales_line->current_price * $sales_line->quantity;
+        }
+        
+        $today_clients = Sale::where('created_at', '>=', $current_date)->distinct()->count();
+
+        $new_clients = Sale::with('client')->whereRelation('client', 'created_at', '>=', $current_date)->distinct()->count();
+
         return view('home', [
-            'bread' => $bread
+            'bread' => $bread,
+            'earnings' => number_format($earnings, 2, ',', '.'),
+            'clients' => $today_clients,
+            'new_clients' => $new_clients,
+            'today_sales' => number_format($today_sales, 2, ',', '.')
         ]);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,51 +9,22 @@
                <div class="row">
                   <div class="col-8">
                      <div class="numbers">
-                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Today's Money</p>
+                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Clientes de hoy</p>
 
                         <h5 class="font-weight-bolder">
-                           $53,000
-                        </h5>
-                        
-                        <p class="mb-0">
-                           <span class="text-success text-sm font-weight-bolder">+55%</span>
-                           since yesterday
-                        </p>
-                     </div>
-                  </div>
-
-                  <div class="col-4 text-end">
-                     <div class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
-                        <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-
-      <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
-         <div class="card">
-            <div class="card-body p-3">
-               <div class="row">
-                  <div class="col-8">
-                     <div class="numbers">
-                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Today's Users</p>
-
-                        <h5 class="font-weight-bolder">
-                           2,300
+                           {{ $clients }}
                         </h5>
 
-                        <p class="mb-0">
+                        {{-- <p class="mb-0">
                            <span class="text-success text-sm font-weight-bolder">+3%</span>
                            since last week
-                        </p>
+                        </p> --}}
                      </div>
                   </div>
 
                   <div class="col-4 text-end">
                      <div class="icon icon-shape bg-gradient-danger shadow-danger text-center rounded-circle">
-                        <i class="ni ni-world text-lg opacity-10" aria-hidden="true"></i>
+                        <i class="ni ni-single-02 text-lg opacity-10" aria-hidden="true"></i>
                      </div>
                   </div>
                </div>
@@ -67,22 +38,22 @@
                <div class="row">
                   <div class="col-8">
                      <div class="numbers">
-                        <p class="text-sm mb-0 text-uppercase font-weight-bold">New Clients</p>
+                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Clientes nuevos</p>
 
                         <h5 class="font-weight-bolder">
-                           +3,462
+                           {{ $new_clients > 0 ? '+'.$new_clients : 0 }}
                         </h5>
 
-                        <p class="mb-0">
+                        {{-- <p class="mb-0">
                            <span class="text-danger text-sm font-weight-bolder">-2%</span>
                            since last quarter
-                        </p>
+                        </p> --}}
                      </div>
                   </div>
 
                   <div class="col-4 text-end">
                      <div class="icon icon-shape bg-gradient-success shadow-success text-center rounded-circle">
-                        <i class="ni ni-paper-diploma text-lg opacity-10" aria-hidden="true"></i>
+                        <i class="fas fa-user-plus text-lg opacity-10" aria-hidden="true"></i>
                      </div>
                   </div>
                </div>
@@ -96,21 +67,50 @@
                <div class="row">
                   <div class="col-8">
                      <div class="numbers">
-                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Sales</p>
+                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Ventas</p>
 
                         <h5 class="font-weight-bolder">
-                           $103,430
+                           {{ $today_sales }}
                         </h5>
 
-                        <p class="mb-0">
+                        {{-- <p class="mb-0">
                            <span class="text-success text-sm font-weight-bolder">+5%</span> than last month
-                        </p>
+                        </p> --}}
                      </div>
                   </div>
 
                   <div class="col-4 text-end">
                      <div class="icon icon-shape bg-gradient-warning shadow-warning text-center rounded-circle">
                         <i class="ni ni-cart text-lg opacity-10" aria-hidden="true"></i>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+
+      <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+         <div class="card">
+            <div class="card-body p-3">
+               <div class="row">
+                  <div class="col-8">
+                     <div class="numbers">
+                        <p class="text-sm mb-0 text-uppercase font-weight-bold">Ganancias</p>
+
+                        <h5 class="font-weight-bolder">
+                           {{ $earnings }}
+                        </h5>
+                        
+                        {{-- <p class="mb-0">
+                           <span class="text-success text-sm font-weight-bolder">+55%</span>
+                           since yesterday
+                        </p> --}}
+                     </div>
+                  </div>
+
+                  <div class="col-4 text-end">
+                     <div class="icon icon-shape bg-gradient-primary shadow-primary text-center rounded-circle">
+                        <i class="ni ni-money-coins text-lg opacity-10" aria-hidden="true"></i>
                      </div>
                   </div>
                </div>
